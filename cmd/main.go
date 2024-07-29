6533c732ba3b41ffce4658e52b7b559d20b6dc94
m-abubakar <mab88s@gmail.com>
2024-07-29 16:48:29 +0500

    fixed formatting

--- cmd/main.go
+++ cmd/main.go
@@ -9,12 +9,10 @@ import (
 
 func main() {
 	addr := ":8080"
-
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/hello", rest.TranslateHandler)
 
 	log.Printf("listening on %s\n", addr)
-
 	log.Fatal(http.ListenAndServe(addr, mux))
 }
